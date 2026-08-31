52b6f3903e942939f3f0111369198fccbb936ff8
xigma <crazyorange12233@outlook.com>
2026-08-31 22:18:31 +0800

    test(assessment): isolate worker entrypoint context

--- services/assessment/src/test/java/com/onlinejudge/assessmentservice/WorkerEntrypointTest.java
+++ services/assessment/src/test/java/com/onlinejudge/assessmentservice/WorkerEntrypointTest.java
@@ -4,10 +4,12 @@ import com.onlinejudge.assessmentservice.worker.AssessmentWorkerLoop;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(properties = "assessment.worker.enabled=true")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class WorkerEntrypointTest {
     @Autowired AssessmentWorkerLoop loop;
     @Test void workerWorkloadRegistersTheQueueDrivingEntrypoint() { assertThat(loop).isNotNull(); }
